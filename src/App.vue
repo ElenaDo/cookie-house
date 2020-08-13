b7c8499389d8bc8212cec618d7c052850ffad121
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2020-08-13 11:58:30 +0200

    door and mailbox added

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,20 @@
                 </div>
               </b-col>
             </b-row>
+            <b-row class="door-row" align-h="end">
+              <b-col cols="4">
+                <div id="door" class="d-flex align-items-center">
+                  <div id="door-handle" class="rounded ml-1">
+                  </div>
+                </div>
+              </b-col>
+              <b-col cols="4" class="pl-0 pt-3">
+                <div id="mailbox" class="pt-2">
+                  <div id="mailbox-cap" class="mx-auto"></div>
+                  <span>Mailbox</span>
+                </div>
+              </b-col>
+            </b-row>
           </div>
         </b-row>
       </div>
@@ -81,7 +95,7 @@ export default {
 #house-base {
   position: relative;
   width: 95%;
-  height: 200px;
+  height: auto;
   background-color: #cccccc;
 }
 .window {
@@ -92,5 +106,35 @@ export default {
   background-color: #d0e1f4;
   transition: background-color 0.5s ease;
   z-index: 50;
+}
+.door-row{
+  min-height: 130px;
+  overflow: visible;
+}
+#door {
+  position: absolute;
+  margin: 0 auto;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 130%;
+  width: 60%;
+  background-color:#f5b465;
+}
+#door-handle {
+  width: 30px;
+  height: 5px;
+  background-color: #b46100;
+}
+#mailbox {
+  width: 60%;
+  height: 50%;
+  background-color: #999;
+  bottom: 50px;
+}
+#mailbox-cap {
+  width: 80%;
+  height: 10%;
+  background-color: #656565;
 }
 </style>
