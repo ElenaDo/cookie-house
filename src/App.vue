5b4222fb149fd0d38ebf7946f30b12db7eba7e70
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2020-08-13 10:06:48 +0200

    the sun added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
+    <b-container fluid="sm">
+      <div class="house-container mx-auto">
+        <b-row align-h="end" class="mx-0">
+          <b-col cols="4">
+            <div id="sun" class="rounded-circle mt-3 mr-3"></div>
+          </b-col>
+        </b-row>
+      </div>
+    </b-container>
   </div>
 </template>
 
@@ -20,5 +29,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.house-container{
+  max-width: 450px;
+}
+#sun {
+  position: relative;
+  width: 100px;
+  height: 100px;
+  background-color: #fbff00;
 }
 </style>
